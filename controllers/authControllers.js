ad9b784e00f3e2867e4cc25e7873e0e9f5e344ba
Fernando <fernandofugihara@gmail.com>
2023-10-16 19:08:43 -0300

    login error corrected

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -7,6 +7,7 @@
 import UserModel from "../models/UserModel.js";
 import { StatusCodes } from "http-status-codes";
 import { hashPassword } from "../utils/tokenUtils.js";
+import "express-async-errors";
 
 import { UnauthenticatedError } from "../errors/customErrors.js";
 import { comparePassword } from "../utils/tokenUtils.js";
@@ -30,32 +31,43 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
 
     const user = await UserModel.findOne({email: req.body.email});
+
+    // Check if the email exists
     if(!user) {
-        throw new UnauthenticatedError("invalid email");
+        throw new UnauthenticatedError("invalid email")
     };
 
     const isPasswordCorrect = await comparePassword(req.body.password, user.password);
+
+    // Check if the email already exists if the password is correct or not
     if(!isPasswordCorrect) {
         throw new UnauthenticatedError("invalid password");
     };
 
-    const token = createJWT(
-        {
-            userId: user._id,
-            role: user.role
-        }
-    );
+    // The front-end will send the token and the server will decode it
+    const token = createJWT({userId: user._id, role: user.role});
 
+    // COOKIE, When we make the post request for the login, a cookie is created and will come back in every request made by this
+    // user. We'll use it to secure the application and the jobs information.
+    // With this statement, the cookie comes back in every request. We're going to use it to authenticate the user, if the token is
+    // valid, we'll allow the user to make requests.
     const oneDay = 1000 * 60 * 60 * 24;
+
+
+    // When a user logged in a cookie and a token gets created with the payload containing the user data. Each user that logged in,
+    // will have a different data in the cookie.
     res.cookie("token", token, {
         httpOnly: true,
         expires: new Date(Date.now() + oneDay),
         secure: process.env.NODE_ENV === "production"
     });
 
-    res.status(StatusCodes.OK).json({
-        msg: "user logged in"
-    });
+    res.status(StatusCodes.OK).json(
+        {
+            msg: "user logged in",
+            user: user
+        }
+    );
 };
 
 // 3- Logout controller GET
